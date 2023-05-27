de100610db62c293dac2d9e2b18f5c54634d56f4
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-05-27 22:29:27 +0100

    Updated Components

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Image from './Group 77.png'
+import './Hero.css'
 
 const Hero = () => {
   return (
-    <div>
-      <img src={Image} alt="PhotoGrid" />
+    <div className='Hero'>
+      <img className='Hero_Img' src={Image} alt="PhotoGrid" />
     </div>
   )
 }
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import logo from './airbnb 1.png'
+import "./Nav.css"
 
 const Nav = () => {
   return (
-    <div>
-      <img src={logo} alt="airbnb_logo" />
+    <div className='Nav'>
+      <img src={logo} className='logo' alt="airbnb_logo" />
     </div>
   )
 }
