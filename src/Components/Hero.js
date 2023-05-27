85bca5295c679d7ae915758202470d687019dfc1
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-05-27 19:22:14 +0100

    updated Components

---
+++ src/Components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from './Group 77.png'
+
+const Hero = () => {
+  return (
+    <div>
+      <img src={Image} alt="PhotoGrid" />
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/Components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from './airbnb 1.png'
+
+const Nav = () => {
+  return (
+    <div>
+      <img src={logo} alt="airbnb_logo" />
+    </div>
+  )
+}
+
+export default Nav
